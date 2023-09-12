a6f12708e0de836169dd5b5f79e2ef5e6492c8d6
peterschefffer <pscheff8@gmail.com>
2023-09-11 18:23:47 -0600

    get google

--- test1.py
+++ test1.py
@@ -1,6 +1,8 @@
 import requests
 print(requests.__version__)
 
+requests.get("http://www.google.com/")
+
 res = requests.get("https://raw.githubusercontent.com/peterschefffer/404lab1/main/test1.py")
 raw = res.text
 print(raw)
